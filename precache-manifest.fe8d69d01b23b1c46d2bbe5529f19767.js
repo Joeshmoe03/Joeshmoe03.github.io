abc159e45b3b04040670396f65778e3e5e063489
Josef <liemjt@comcast.net>
2024-11-03 14:01:43 -0500

    Updates

---
+++ precache-manifest.fe8d69d01b23b1c46d2bbe5529f19767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d2fb7d0122b330febc2342fae57d80",
+    "url": "/Joeshmoe03/Joeshmoe03.github.io.git/index.html"
+  },
+  {
+    "revision": "b1044fa478e3d9896b74",
+    "url": "/Joeshmoe03/Joeshmoe03.github.io.git/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/Joeshmoe03/Joeshmoe03.github.io.git/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Joeshmoe03/Joeshmoe03.github.io.git/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "b1044fa478e3d9896b74",
+    "url": "/Joeshmoe03/Joeshmoe03.github.io.git/static/js/main.704f432d.chunk.js"
+  },
+  {
+    "revision": "aeef17b985ddd800711c",
+    "url": "/Joeshmoe03/Joeshmoe03.github.io.git/static/js/runtime~main.bee8c702.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Joeshmoe03/Joeshmoe03.github.io.git/static/media/logo.06e73328.svg"
+  }
+]);
